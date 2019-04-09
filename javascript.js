03feb29652cea26604f130806575dd50f74164ed
manuel-rahm <45931464+manuel-rahm@users.noreply.github.com>
2019-04-09 11:01:29 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,15 +2,18 @@ $('.modal_close').on('click', function(){
     $('.modal_content').hide();
     $('.modal_close').hide();
     $('.modal_open').show();
+    $('body:not(.modal)').css("background-color", "white").css("opacity", "1");
 });
 
 $('.modal_open').on('click', function(){
     $('.modal_content').show();
     $('.modal_close').show();
     $('.modal_open').hide();
+    $('body').css("background-color", "darkgrey").css("opacity", "0.8");
+    $(".modal-content").css("opacity", "1");
 });
 $(function() {
-    $( ".modal_content" ).dialog({
+    $(".modal_content").dialog({
         modal: true,
         draggable: false,
         resizable: false,
